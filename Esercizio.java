e064daf45b1044a28dac9a035f7f7a4ef9716997
Antonio-Milella <antonio.milella@marconibari.edu.it>
2025-03-20 20:06:16 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -2,6 +2,9 @@
 
 //Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
 
 // Classe principale, con metodo main
 class Esercizio {
@@ -9,18 +12,49 @@ class Esercizio {
     public static void main(String args[])
     {
         //Variabili del programma
-        String nome;
+        String iFn, oFn, riga;
 
         //Creo l'oggetto in per l'input da tastiera
+
+        File FR = new File (iFn);
+        File FW = new File (oFn);
+        
         Scanner in = new Scanner( System.in );
+        Scanner inFile = new Scanner(new FileReader(FR));
+        Scanner outFile = new Scanner(new FileWriter(FW));
 
         //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        System.out.println("Inserisci nome del file di input: ");
+        iFn = in.nextLine();
+        System.out.println("Inserisci nome del file di output: ");
+        oFn = in.nextLine();
+        
+        do{
+            riga = inFile.nextLine();
+            riga = inverti(riga);
+            inFile.close();
+            scriviFile(riga,iFn);
+        }while(inFile.hasNextLine());
+        in.close();
     }
+
+    public static String inverti(String riga){
+        int n, i;
+        String agir;
+
+        n = riga.length();
+        agir = "";
+        for (i = n-1; i>=0; i--){
+            agir = agir + riga.charAt(i);
+        }    
+        return agir;
+    }
+
+    public static void scriviFile(String riga, String nomeFile) {
+        outFile.write(System.lineSeparator(riga));
+        outFile.close();
+    }
+    
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
